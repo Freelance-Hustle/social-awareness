b05750591053b10ffec54f61f1d6e02c46ad3b0a
wazimuhb <hesbonosoro1@gmail.com>
2022-02-02 12:10:31 +0300

    some final touches

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
-
-import './Footer.scss';
+import {
+	Box,
+	Container,
+	Row,
+	Column,
+	FooterLink,
+	Heading,
+} from './FooterStyles';
 
 const Footer: React.FC = () => {
 	return (
-		<footer className="footer">
-			<div className="links">
-				<h1>Quick Links</h1>
-				<a href="/">Home</a>
-				<a href="/">Products</a>
-				<a href="/">About Us</a>
-			</div>
-			<div className="icons">
-				<h1>Follow Us</h1>
-			</div>
-			<div className="news">
-				<h1>Our Newsletter</h1>
-				<p>Get latest posts, best deals and any other relevant information.</p>
-			</div>
-		</footer>
+		<Box>
+			<h1 style={{ color: 'green', textAlign: 'center', marginTop: '-50px' }}>
+				Social Awareness: make someone aware
+			</h1>
+			<Container>
+				<Row>
+					<Column>
+						<Heading>About Us</Heading>
+						<FooterLink href="#">Aim</FooterLink>
+						<FooterLink href="#">Vision</FooterLink>
+						<FooterLink href="#">Testimonials</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Services</Heading>
+						<FooterLink href="#">Developing</FooterLink>
+						<FooterLink href="#">Internships</FooterLink>
+						<FooterLink href="#">Coding</FooterLink>
+						<FooterLink href="#">Teaching</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Contact Us</Heading>
+						<FooterLink href="#">Developer</FooterLink>
+						<FooterLink href="#">Designer</FooterLink>
+						<FooterLink href="#">Database</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Follow Us</Heading>
+						<FooterLink href="#">
+							<i className="fab fa-facebook-f">
+								<span style={{ marginLeft: '10px' }}>Facebook</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-instagram">
+								<span style={{ marginLeft: '10px' }}>Instagram</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-twitter">
+								<span style={{ marginLeft: '10px' }}>Twitter</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-youtube">
+								<span style={{ marginLeft: '10px' }}>Youtube</span>
+							</i>
+						</FooterLink>
+					</Column>
+				</Row>
+			</Container>
+		</Box>
 	);
 };
-
 export default Footer;
